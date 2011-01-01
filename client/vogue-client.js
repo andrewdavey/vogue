fbbeef4ec5af7b8d59458c15c78b2abafbebd80b
Andrew Davey <andrew@equin.co.uk>
2011-01-01 11:10:53 +0000

    Initial commit

---
+++ client/vogue-client.js
@@ -0,0 +1,94 @@
+// Vogue - Client
+// Copyright (c) 2011 Andrew Davey (andrew@equin.co.uk)
+//
+// Note that all occurances of {port} in this script will be replaced with the port of the 
+// HTTP server hosted by Node.
+
+(function() {
+
+loadScripts({
+  jQuery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js',
+  io: 'http://localhost:{port}/socket.io/socket.io.js'
+}, vogue);
+
+function vogue() {
+  var stylesheets = getLocalStylesheets();
+  var socket = new io.Socket('localhost', { port: {port} });
+  socket.on('connect', watchAllStylesheets);
+  socket.on('message', handleMessage);
+  socket.connect();
+
+  function watchAllStylesheets() {
+    var origin = document.location.protocol + '//' + document.location.host;
+    var skipPrefixLength = origin.length;
+    for (var href in stylesheets) {
+      // linkElement.href is resolved by the browser to the full URL.
+      // (unlike reading the actual attribute value.)
+      // Very handy; we can easily get the full path segment of the URL
+      // by skipping the current document's location origin length.
+      var path = stylesheets[href].href.substr(skipPrefixLength);
+      socket.send('watch ' + path);
+    }
+  }
+
+  function handleMessage(message) {
+    var match = message.match(/^update (.*)$/);
+    if (match) {
+      var href = match[1];
+      reloadStylesheet(href);
+    }
+  }
+
+  function reloadStylesheet(href) {
+    var newHref = href 
+      + (href.indexOf('?') >= 0 ? '&' : '?') 
+      + '_vogue_nocache=' 
+      + (new Date()).getTime();
+    $(stylesheets[href]).attr('href', newHref);
+  }
+  
+  function getLocalStylesheets() {
+    var links = $('link[type=text/css][href]').filter(isLocal);
+    var origin = document.location.protocol + '//' + document.location.host;
+    var stylesheets = {};
+    links.each(function() {
+      var href = this.href.substr(origin.length)
+      stylesheets[href] = this;
+    });
+    return stylesheets;
+
+    function isLocal() {
+      var href = $(this).attr('href');
+      return !href.match(/^https?:/);
+    }
+  }
+}
+
+function loadScripts(scripts, loadedCallback) {
+  var srcs = [];
+  for (var property in scripts) {
+    if (!(property in window)) srcs.push(scripts[property]);
+  }
+
+  var count = srcs.length;
+  if (count == 0) loadedCallback();
+  srcs.forEach(function(src) {
+    loadScript(src, function() {
+      count--;
+      if (count == 0) loadedCallback();
+    });
+  });
+}
+
+function loadScript(src, loadedCallback) {
+  var script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('src', src);
+  script.onload = loadedCallback; // Chrome
+  script.onreadystatechange = function () { // IE?
+    if (this.readyState == 'complete') loadedCallback();
+  }
+  document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+})();
---
+++ vogue.js
@@ -0,0 +1,144 @@
+/* vogue.js
+ * A tool for web developers. Vogue watches for changes to CSS files and
+ * informs the web browser using them to reload those stylesheets.
+ * 
+ * Created by Andrew Davey ~ http://aboutcode.net 
+ *
+ * Vogue runs on nodeJS and uses socket.io for real-time communication between
+ * browser and server.
+ */
+
+var http = require('http'),
+    fs   = require('fs'),
+    path = require('path'),
+    io   = require('socket.io');
+
+var defaultOptions = {
+  port: '8001',
+  dir: ''
+};
+var options = parseOptions(process.argv, defaultOptions);
+options.port = parseInt(options.port);
+options.dir = path.join(process.cwd(), options.dir);
+
+console.log('Watch directory: ' + options.dir);
+
+
+
+var server = http.createServer(onWebRequest);
+server.listen(options.port);
+var socket = io.listen(server);
+var clients = [];
+var watchedFiles = {};
+
+socket.on('connection', function(client) {
+  var vogueClient = new VogueClient(client);
+  clients.push(vogueClient);
+  client.on('message', vogueClient.handleMessage.bind(vogueClient));
+  client.on('disconnect', vogueClient.disconnect.bind(vogueClient));
+});
+
+
+function onWebRequest(request, response) {
+  if (request.url === '/') {
+    sendAboutPage(response);
+  } else if (request.url === '/vogue-client.js') {
+    sendVogueClient(response);
+  }
+}
+
+
+function VogueClient(clientSocket) {
+  this.socket = clientSocket;
+  this.watchedFiles = {};
+}
+VogueClient.prototype.handleMessage = function(message) {
+  var match = message.match(/^watch (.*)$/);
+  if (!match) return;
+  this.watchFile(match[1]);  
+};
+VogueClient.prototype.watchFile = function(href) {
+  var filename = path.join(options.dir, href.substr(1));
+  fs.stat(filename, function(err, stats) {
+    this.watchedFiles[filename] = {
+      href: href,
+      mtime: stats.mtime
+    };
+    startWatching(filename);
+  }.bind(this));
+};
+VogueClient.prototype.updateFile = function(filename) {
+  var fileInfo = this.watchedFiles[filename];
+  if (fileInfo) {
+    fs.stat(filename, function(err, stats) {
+      if (fileInfo.mtime < stats.mtime) {
+        this.socket.send('update ' + fileInfo.href);
+        fileInfo.mtime = stats.mtime;
+      }
+    }.bind(this));
+  }
+};
+VogueClient.prototype.disconnect = function() {
+  for (var filename in this.watchedFiles) {
+    stopWatching(filename);
+  }
+};
+
+function startWatching(filename) {
+  if (filename in watchedFiles) {
+    watchedFiles[filename]++;
+  } else {
+    fs.watchFile(filename, { persistent: true, interval: 50 }, function() {
+      clients.forEach(function(client) {
+        client.updateFile(filename);
+      });
+    });
+    watchedFiles[filename] = 1;
+    console.log('File watched: ' + filename);
+  }
+}
+
+function stopWatching(filename) {
+  if (--watchedFiles[filename] == 0) {
+    delete watchedFiles[filename];
+    fs.unwatchFile(filename);
+    console.log('File unwatched: ' + filename);
+  }
+}
+
+function sendAboutPage(response) {
+  fs.readFile(__dirname + '/client/about.htm', function(e, fileData) {
+    var html = fileData.toString();
+    html = html.replace(/\{port\}/g, options.port.toString());
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(html);
+    response.end();
+  });
+}
+
+function sendVogueClient(response) {
+  fs.readFile(__dirname + '/client/vogue-client.js', function(e, fileData) {
+    var script = fileData.toString();
+    script = script.replace(/\{port\}/g, options.port.toString());
+    response.writeHead(200, { 'Content-Type': 'text/javascript' });
+    response.write(script);
+    response.end();
+  });
+}
+
+function parseOptions(argv, defaults) {
+  var options = {}, nextArgName;
+  for (var p in defaults) {
+    if (defaults.hasOwnProperty(p)) options[p] = defaults[p];
+  }
+  for (var i = 0; i < argv.length; i++) {
+    if (nextArgName) {
+      options[nextArgName] = argv[i];
+      nextArgName = null;
+    }
+    if (argv[i].match(/^-./)) {
+      nextArgName = argv[i].substr(1);
+    }
+  }
+  return options;
+}
