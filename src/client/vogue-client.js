c7a1416aebdf34924afc63bd8643ddfea36b428e
Bernhard K. Weisshuhn <bkw@codingforce.com>
2011-04-01 10:58:55 +0200

    locate stylesheets by rel=stylesheet, not type

--- src/client/vogue-client.js
+++ src/client/vogue-client.js
@@ -41,7 +41,7 @@ function vogue() {
   }
   
   function getLocalStylesheets() {
-    var links = $('link[type="text/css"][href]').filter(isLocal);
+    var links = $('link[rel="stylesheet"][href]').filter(isLocal);
     var stylesheets = {};
     links.each(function() {
       // Match hrefs against stylesheet bases we know of
